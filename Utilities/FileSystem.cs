4f91f7acc1ae52bcdf5264347d5a1fa82ab17448
Yaseen Alali <108293462+0x43ow@users.noreply.github.com>
2023-11-16 12:31:12 +0200

    Docs for file system functions

--- Utilities/FileSystem.cs
+++ Utilities/FileSystem.cs
@@ -2,6 +2,16 @@
 {
     public class FileSystem
     {
+        /// <summary>
+        /// Retrieves the path of the first audio file in the specified directory.
+        /// </summary>
+        /// <param name="path">The path of the directory to search for audio files.</param>
+        /// <returns>
+        /// The full path of the first audio file found, or an empty string if no audio files are present.
+        /// </returns>
+        /// <remarks>
+        /// Supported audio file extensions: mp3, wav, flac, opus, ogg, aac.
+        /// </remarks>
         public static string GetFirstAudioFile(string path)
         {
             try
@@ -12,15 +22,7 @@
                     .Where(file => audioExtensions.Any(ext => file.EndsWith(ext, StringComparison.OrdinalIgnoreCase)))
                     .ToList();
 
-                if (audioFiles.Count > 0)
-                {
-                    return audioFiles.First();
-                }
-                else
-                {
-                    return "";
-                }
-
+                return audioFiles.Count > 0 ? audioFiles.First() : "";
             }
             catch (Exception ex)
             {
@@ -29,6 +31,14 @@
             }
         }
 
+
+        /// <summary>
+        /// Gets the total count of items (files and subdirectories) in the specified directory and its subdirectories.
+        /// </summary>
+        /// <param name="path">The path of the directory to get the item count for.</param>
+        /// <returns>
+        /// The total count of items in the directory and its subdirectories, or -1 if the directory doesn't exist.
+        /// </returns>
         public static int GetDirectoryItemsCount(string path)
         {
             try
@@ -54,6 +64,14 @@
             }
         }
 
+
+        /// <summary>
+        /// Deletes all files and subdirectories within the specified directory.
+        /// </summary>
+        /// <param name="path">The path of the directory to delete files and subdirectories from.</param>
+        /// <returns>
+        /// True if the operation succeeds; otherwise, false.
+        /// </returns>
         public static bool DeleteFilesAndSubdirectories(string path)
         {
             try
@@ -69,6 +87,7 @@
                 {
                     subdirectory.Delete(true);
                 }
+
                 return true;
             }
             catch (Exception ex)
@@ -78,14 +97,22 @@
             }
         }
 
+
+        /// <summary>
+        /// Lists all audio files within the specified directory and its subdirectories.
+        /// </summary>
+        /// <param name="path">The path of the directory to list audio files from.</param>
+        /// <returns>
+        /// An array of strings representing the paths of the discovered audio files.
+        /// </returns>
+        /// <remarks>
+        /// Supported audio file extensions: mp3, wav, flac, opus, ogg, aac.
+        /// </remarks>
         public static string[] ListAudioFiles(string path)
         {
             try
             {
-                string[] mediaExtensions =
-{
-                    "mp3", ".wav", ".flac", "opus", ".ogg", ".aac"
-                };
+                string[] mediaExtensions = { "mp3", ".wav", ".flac", "opus", ".ogg", ".aac" };
 
                 return Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
                     .Where(file => mediaExtensions.Contains(Path.GetExtension(file).ToLower()))
@@ -93,13 +120,20 @@
                     .Select(path => path.Replace(Path.DirectorySeparatorChar, '/'))
                     .ToArray();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.Write(ex.Message);
                 return new string[0];
             }
         }
 
+
+        /// <summary>
+        /// Creates a unique directory within the 'DownloadedFiles' directory and returns its unique identifier.
+        /// </summary>
+        /// <returns>
+        /// A unique identifier for the newly created directory, or an empty string if creation fails.
+        /// </returns>
         public static string CreateUniqueDirectory()
         {
             try
@@ -113,35 +147,52 @@
                 Console.WriteLine(e.Message);
                 return "";
             }
-
         }
 
+
+        /// <summary>
+        /// Creates the 'DownloadedFiles' directory if it does not already exist.
+        /// </summary>
+        /// <returns>
+        /// True if the directory already exists or is successfully created; otherwise, false.
+        /// </returns>
         public static bool CreateDownloadedFilesDirectory()
         {
-            try {
+            try
+            {
                 Directory.CreateDirectory("DownloadedFiles");
+
                 return true;
             }
-            catch(Exception ex) { 
-                Console.WriteLine (ex.Message);
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
                 return false;
             }
         }
 
+
+        /// <summary>
+        /// Checks if the 'DownloadedFiles' directory exists and creates it if not.
+        /// </summary>
+        /// <returns>
+        /// True if the directory already exists or is successfully created; otherwise, false.
+        /// </returns>
         public static bool CheckDownloadedFilesDirectoryAndCreateIfNotExisting()
         {
             try
             {
-                if (Directory.Exists("DownloadedFles"))
+                if (Directory.Exists("DownloadedFiles"))
                 {
                     return true;
                 }
-                else { 
+                else
+                {
                     bool created = CreateDownloadedFilesDirectory();
                     return created;
                 }
             }
-            catch( Exception ex )
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 return false;
